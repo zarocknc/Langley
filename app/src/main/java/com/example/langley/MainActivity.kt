eb23ba7c3b1df94b091835281689eca52fe6c067
zarocknc <sebastian_nicolaschong@protonmail.com>
2022-11-01 16:24:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/langley/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.langley
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+
+
+    }
+}
